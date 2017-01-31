238f0be24b8ae271656bad8712d35b3609de8cd3
chauncey <cchauncey@gmail.com>
2017-01-30 18:31:10 -0600

    git alias cleanup

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,15 +7,15 @@ then
 fi
 
 alias unadd='git reset HEAD'
-alias ac='!git add . && git commit -am'
+alias ac='git add . && git commit -am'
 alias la='!git config -l | grep alias | cut -c 7-'
 alias l=log --pretty-format:"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=short
-alias b="!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'"
-alias clean-merged='!git branch --merged | grep -v \"\\*\" | xargs -n 1 git branch -d'
+alias b="git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'"
+alias clean-merged='git branch --merged | grep -v \"\\*\" | xargs -n 1 git branch -d'
 alias rao='git remote add origin'
-alias fu='!git fetch && git fetch upstream'
+alias fu='git fetch && git fetch upstream'
 alias fa='git fetch --all'
-alias fairy-pick='!git fetch && git cherry-pick'
+alias fairy-pick='git fetch && git cherry-pick'
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
